e76352b508dd0e613bfae10a8370386f74cfc35f
joe <joe@imac.home>
2026-04-09 14:01:29 +0100

    Display landing page content on home route (/)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import Navigation from './landing/components/Navigation';
+import HeroSection from './landing/components/Hero/HeroSection';
+import Testimonial from './landing/components/Testimonial/Testimonial';
+import TextDescription from './landing/components/TextDescription/TextDescription';
+import CTAButtons from './landing/components/CTAButtons/CTAButtons';
+import Services from './landing/components/Services/Services';
+import Reviews from './landing/components/Reviews/Reviews';
+
 export default function Home() {
   return (
-    <div>
-      {/* Blank home page */}
-    </div>
+    <>
+      <Navigation />
+      <div className="min-h-screen bg-white text-gray-900">
+        <HeroSection />
+        <Testimonial />
+        <TextDescription />
+        <CTAButtons />
+        <Services />
+        <Reviews />
+      </div>
+    </>
   );
 }
