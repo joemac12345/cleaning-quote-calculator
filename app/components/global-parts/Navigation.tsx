1386ee0ddf3c92d4de5c9368fc4e2b1b1888e00a
joe <joe@imac.home>
2026-04-12 13:51:41 +0100

    Increase Navigation top margin to mt-10

--- app/components/global-parts/Navigation.tsx
+++ app/components/global-parts/Navigation.tsx
@@ -20,7 +20,7 @@ export default function Navigation() {
   }
 
   return (
-    <nav className="bg-white mt-6">
+    <nav className="bg-white mt-10">
       <div className="mx-auto max-w-[700px] px-4 py-2">
         {/* Header */}
         <div className="flex items-center gap-3">
